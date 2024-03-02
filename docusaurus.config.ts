c4ae927adc3ac87272695f34c77115592dacfe3b
Caelan Sayler <git@caesay.com>
2024-03-02 21:44:48 +0000

    clean config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,34 +6,19 @@ const config: Config = {
   title: 'Velopack',
   tagline: 'Dinosaurs are cool',
   favicon: 'favicon/favicon.ico',
-  // trailingSlash: true,
-
-  // Set the production url of your site here
   url: 'https://docs.velopack.io/',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'velopack', // Usually your GitHub org/user name.
-  projectName: 'velopack.docs', // Usually your repo name.
-
+  organizationName: 'velopack',
+  projectName: 'velopack.docs',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
   markdown: {
     format: 'detect'
   },
-
   presets: [
     [
       'classic',
@@ -41,25 +26,26 @@ const config: Config = {
         docs: {
           sidebarPath: './sidebars.ts',
           routeBasePath: '/',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/velopack/velopack.docs/tree/master/',
+          showLastUpdateTime: true,
+          showLastUpdateAuthor: true,
+
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: './src/css/custom.css',
         },
+        gtag: {
+          trackingID: 'G-YM5CQ7KMDT',
+          anonymizeIP: true,
+        }
       } satisfies Preset.Options,
     ],
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: 'logo/opengraph.png',
     navbar: {
       // title: 'Velopack',
