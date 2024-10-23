40b3a6f6a5f2560124f8463804476bc1c167288d
Caelan Sayler <git@caesay.com>
2024-10-23 21:17:31 +0100

    Try running typedoc in a temp dir

--- generator/JavaScriptReference.cs
+++ generator/JavaScriptReference.cs
@@ -7,44 +7,45 @@ public static class JavaScriptReference
         Directory.Delete(jsReferenceOutput, true);
         Directory.CreateDirectory(jsReferenceOutput);
 
-        var workDir = Path.Combine(AppContext.BaseDirectory, "ref_JS");
-        if (Directory.Exists(workDir)) {
+        var workDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+
+        try {
+            Directory.CreateDirectory(workDir);
+
+            await Util.StartShellProcess("npm", ["pack", "velopack"], workDir);
+            var tarball = Directory.GetFiles(workDir, "*.tgz").Single();
+            await Util.StartShellProcess("tar", ["-xzf", tarball], workDir);
+
+            var packageDir = Path.Combine(workDir, "package");
+            await Util.StartShellProcess("npm", ["i", "typedoc", "typedoc-plugin-markdown"], packageDir);
+            await Util.StartShellProcess(
+                "npx",
+                [
+                    "typedoc",
+                    "--out",
+                    Path.GetFullPath(jsReferenceOutput),
+                    "--plugin",
+                    "typedoc-plugin-markdown",
+                    "--flattenOutputFiles",
+                    "--readme",
+                    "none",
+                    "--entryFileName",
+                    "index",
+                    "--hidePageHeader",
+                    "--hideBreadcrumbs",
+                    "--indexFormat",
+                    "table",
+                    "--parametersFormat",
+                    "table",
+                    "--disableSources",
+                    "lib/index.d.ts"
+                ],
+                packageDir);
+
+            var indexMd = Path.Combine(jsReferenceOutput, "index.md");
+            await Util.SetPageSidebarOverview(indexMd);
+        } finally {
             Directory.Delete(workDir, true);
         }
-
-        Directory.CreateDirectory(workDir);
-
-        await Util.StartShellProcess("npm", ["pack", "velopack"], workDir);
-        var tarball = Directory.GetFiles(workDir, "*.tgz").Single();
-        await Util.StartShellProcess("tar", ["-xzf", tarball], workDir);
-
-        var packageDir = Path.Combine(workDir, "package");
-        await Util.StartShellProcess("npm", ["i", "typedoc", "typedoc-plugin-markdown"], packageDir);
-        await Util.StartShellProcess(
-            "npx",
-            [
-                "typedoc",
-                "--out",
-                Path.GetFullPath(jsReferenceOutput),
-                "--plugin",
-                "typedoc-plugin-markdown",
-                "--flattenOutputFiles",
-                "--readme",
-                "none",
-                "--entryFileName",
-                "index",
-                "--hidePageHeader",
-                "--hideBreadcrumbs",
-                "--indexFormat",
-                "table",
-                "--parametersFormat",
-                "table",
-                "--disableSources",
-                "lib/index.d.ts"
-            ],
-            packageDir);
-
-        var indexMd = Path.Combine(jsReferenceOutput, "index.md");
-        await Util.SetPageSidebarOverview(indexMd);
     }
 }
