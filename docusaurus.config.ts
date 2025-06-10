79b6cddecc1748a9f59775f25832af37fd4f3d6b
Kevin Bost <kitokeboo@gmail.com>
2025-06-10 12:56:26 -0700

    Adding category redirect

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -30,6 +30,7 @@ const config: Config = {
       redirects: [
         { from: '/packaging/rid', to: '/packaging/runtime' },
         { from: '/troubleshooting/cli', to: '/reference/cli' },
+        { from: '/category/quick-start', to: '/category/getting-started' },
       ],
     }],
   ],
